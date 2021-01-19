a283f807acdfcf0533c24a9cdbcf09d5511c0802
cthyuii <74233671+cthyuii@users.noreply.github.com>
2021-01-19 15:48:57 +0800

    Added in a comment for all requirements for program.cs in program.cs.

--- SafeEntryAppThingyAssignment/Program.cs
+++ SafeEntryAppThingyAssignment/Program.cs
@@ -9,6 +9,77 @@ namespace SafeEntryAppThingyAssignment
         // Student Name : Macarius Dai, Tan Jian Xu
         // Module Group : T01
         //============================================================
+        /*===General===
+        1) Load Person and Business Location Data
+            1.load both given CSV and populate two lists
+        2) Load SHN Facility Data
+            1.call API and populate a list
+        3) List all Visitors
+        4) List Person Details
+            1. prompt user for name
+            2. search for person
+            3. list person details including TravelEntry and SafeEntry details
+            i. if resident, display TraceTogetherToken details
+        ===SafeEntry/TraceTogether===
+        5) Assign/Replace TraceTogether Token
+            1. prompt user for name
+            2. search for resident name
+            3. create and assign a TraceTogetherToken object if resident has no existing token
+            4. replace token if it meets the criteria stipulated in the background brief
+        6) List all Business Locations
+        7) Edit Business Location Capacity
+            1. prompt user to enter details
+            2. search for business location
+            3. prompt user to edit maximum capacity
+        8) SafeEntry Check-in
+            1. prompt user for name
+            2. search for person
+            3. list all business locations
+            4. prompt user to select for business location to check-in
+            5. create SafeEntry object if the location is not full, and increase visitorsNow count
+            6. add SafeEntry object to person
+        9) SafeEntry Check-out
+            1. prompt user for name
+            2. search for person
+            3. list SafeEntry records for that person that have not been checked-out
+            4. prompt user to select record to check-out
+            5. call PerformCheckOut() to check-out, and reduce visitorsNow by 1
+        ===TravelEntry===
+        10) List all SHN Facilities
+        11) Create Visitor
+            1. prompt user for details
+            2. create Visitor object
+        12) Create TravelEntry Record
+            1. prompt user for name
+            2. search for person
+            3. prompt user for details (last country of embarkation, entry mode)
+            4. create TravelEntry object
+            5. call CalculateSHNDuration() to calculate SHNEndDate based on criteria given in the background brief
+            6. list SHN facilities if necessary, for user to select
+            7. assign chosen SHN facility if necessary, and reduce the vacancy count
+            8. call AddTravelEntry() in Person to assign the TravelEntry object
+        13) Calculate SHN Charges
+            1. prompt user for name
+            2. search for person
+            3. retrieve TravelEntry with SHN ended and is unpaid
+            4. call CalculateSHNCharges() to calculate the charges based on the criteria provided in the background brief
+                i. Note: To add 7% GST
+            5. prompt to make payment
+            6. change the isPaid Boolean value
+        ▪ Validations (and feedback)
+        ▪ The program should handle all invalid entries by the user
+        e.g. invalid option, invalid year, invalid month, invalid day, etc.
+        ▪ If user made a mistake in the entry, the program should inform the user via appropriate feedback
+        ▪ The program is to display appropriate feedback messages (e.g., successful or not successful)
+        3. ADVANCED FEATURES
+        You are required to do all the advanced features below.
+        3.1 Contact Tracing Reporting
+        • Given a date/time and business name, generate a list of persons that are checked-in at that location and period.
+        • Export a CSV with details of their visit (e.g., check-in time, check-out time)
+        3.2 SHN Status Reporting
+        • Given a date, generate a csv report of all travellers serving their SHN, their SHN end date, and where they are serving their SHN.
+        3.3 Other Possible Features
+        • You may gain up to 5 bonus marks if you propose and successfully implement an additionalfeature. Check with your tutor with your idea before implementing.*/
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
