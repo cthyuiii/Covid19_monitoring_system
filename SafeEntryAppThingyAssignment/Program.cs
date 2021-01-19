e41caf845246f8e92dd33b2f541f2979daa4b9cf
cthyuii <74233671+cthyuii@users.noreply.github.com>
2021-01-19 13:24:42 +0800

    Add project files.

---
+++ SafeEntryAppThingyAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SafeEntryAppThingyAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SafeEntryAppThingyAssignment/SafeEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeEntryAppThingyAssignment
+{
+    class SafeEntry
+    {
+    }
+}
